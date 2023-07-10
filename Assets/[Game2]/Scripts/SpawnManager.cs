3f45fe5c6c9cf22ab2d55f9c9b749033dc76a079
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-07-10 13:42:43 +0300

    Challenge 2 Accepted

--- Assets/[Game2]/Scripts/SpawnManager.cs
+++ Assets/[Game2]/Scripts/SpawnManager.cs
@@ -7,25 +7,23 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
-    private float startDelay = 2f;
-    private float spawnInterval = 1.5f;
+    private float spawnTime = 2f;
+    private float spawnRate = 1.5f;
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        InvokeRepeating("SpawnAnimal", spawnTime, spawnRate);
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if(Input.GetKeyDown(KeyCode.S)) 
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            SpawnRandomAnimal();
+            SpawnAnimal();
         }
-        */
     }
 
-    void SpawnRandomAnimal()
+    private void SpawnAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
