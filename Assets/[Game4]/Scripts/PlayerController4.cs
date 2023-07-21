c1bafcd3d1106fac7d9393de9271fe875a043ad4
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-07-21 11:40:16 +0300

    Lesson 3.3 Done
    New Functionality:
    The player starts the scene with a fast-paced running animation
    When the player jumps, there is a jumping animation
    When the player crashes, the player falls over
    New Concepts and Skills:
    Animation Controllers
    Animation States, Layers, and Transitions
    Animation parameters
    Animation programming
    SetTrigger(), SetBool(), SetInt()
    Not (!) operator
    Next Lesson:
    We’ll really polish this game up to make it look nice using particles and sound effects!

--- Assets/[Game4]/Scripts/PlayerController4.cs
+++ Assets/[Game4]/Scripts/PlayerController4.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController4 : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier = 2;
     public bool isOnGround = true;
@@ -12,16 +13,18 @@ public class PlayerController4 : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -34,6 +37,8 @@ public class PlayerController4 : MonoBehaviour
         {
             Debug.Log("Game over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
         
     }
