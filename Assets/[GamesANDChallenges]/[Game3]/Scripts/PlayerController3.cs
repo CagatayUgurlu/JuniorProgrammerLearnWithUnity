f6a1d6ca138c524322c8c5ea0ab9b703e513e42c
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-07-23 14:12:26 +0300

    Lab 3 and Quiz 3 done

---
+++ Assets/[GamesANDChallenges]/[Game3]/Scripts/PlayerController3.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3 : MonoBehaviour
+{
+    private float speed = 10f;
+    private Rigidbody playerRb;
+    private float zBound = 5f;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        ConstraintPlayerPosition();
+    }
+    private void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+    private void ConstraintPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+
+}
