0bbd8de75af35ffd1c41ffabee0348ab8749edce
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-12-22 22:42:51 +0300

    Scripted Center Of mass

--- Assets/[GamesANDChallenges]/[Game]/Scripts/PlayerController.cs
+++ Assets/[GamesANDChallenges]/[Game]/Scripts/PlayerController.cs
@@ -12,10 +12,12 @@ public class PlayerController : MonoBehaviour
     private float verticalInput;
 
     private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
 
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     void FixedUpdate()
