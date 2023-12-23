dfc5cc1aabbc9e022885fd366c6c936058a71f57
CagatayUgurlu <cagatayugurlu329@gmail.com>
2023-12-23 22:03:35 +0300

    speed float changed to int

--- Assets/[GamesANDChallenges]/[Game]/Scripts/PlayerController.cs
+++ Assets/[GamesANDChallenges]/[Game]/Scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
         playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
         // Rotates the car based on horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
-        speed = (playerRb.velocity.magnitude * 2.237f); // For kph, change to 3.6
+        speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f); // For kph, change to 3.6
         speedometerText.SetText("Speed: " + speed + "mph");
     }
 }
